b0f5758f99f9ad5948c05da1bc69d09f51faa9e0
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-03-07 16:12:03 +0900

    server application logs: pino

--- playground/guide-node-js-logging/index.js
+++ playground/guide-node-js-logging/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use((req, res, next) => {
- console.log('%O', req);
- next();
-});
+app.use(expressLogger);
 
 app.get('/', (req, res) => {
- res.send('Hello World');
+  logger.debug('Calling res.send');
+  res.send('Hello World');
 });
 
 app.listen(PORT, () => {
- console.log('Server running on port %d', PORT);
+  logger.info('Server running on port %d', PORT);
 });
