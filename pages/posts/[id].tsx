4c1f9db1b40b2b34eb57a354572890a00ff1f49c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-12-30 03:40:06 +0900

    add siteTitle in title

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,4 @@
-import Layout from "../../components/layout";
+import Layout, { siteTitle } from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
@@ -17,7 +17,9 @@ export default function Post({
   return (
     <Layout>
       <Head>
-        <title>{postData.title}</title>
+        <title>
+          {siteTitle} {postData.title}
+        </title>
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
